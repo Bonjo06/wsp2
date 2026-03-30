00cad96fd68cd787e3f69d96e637b6fc979cedf4
Benjamin Alejandro Mella Cordero <167372344+Bonjo06@users.noreply.github.com>
2026-03-30 01:15:32 -0300

    Delete wasap2/chat directory

--- wasap2/chat/src/main/java/com/wasap2/chat/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wasap2.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wasap2.chat.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
